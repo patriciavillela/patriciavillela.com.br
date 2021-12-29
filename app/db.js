f52554f13a20755adc7eb6e50c26b06ab7cacd0b
Patrick Villela Ocaña Bruno <patrickvob@gmail.com>
2021-12-29 06:34:57 -0300

    Removes console.log

--- app/db.js
+++ app/db.js
@@ -10,7 +10,6 @@ async function getContent() {
 async function createContent(obj) {
 	insert = "INSERT INTO content(page,element_id,gender,language,string,depth) values('"+obj.page+"','"+obj.element_id+"','"+obj.gender+"','"+obj.language+"','"+obj.string+"',"+obj.depth+");";
 	insert = insert.replace("'NULL'","null");
-	console.log(insert);
 	await client.query(insert);
 }
 
