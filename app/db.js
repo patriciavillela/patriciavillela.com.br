cee3fe7df52418c2c91721cb661e6ef4c5a8b4e4
Patrick Villela Ocaña Bruno <patrickvob@gmail.com>
2021-12-29 04:37:44 -0300

    Adds admin page

--- app/db.js
+++ app/db.js
@@ -4,7 +4,14 @@ client.connect();
 
 async function getContent() {
 	content = await client.query('SELECT * from content');
-	return content.rows;
+	return content;
 }
 
-module.exports = { getContent };
+async function createContent(obj) {
+	insert = "INSERT INTO content(page,element_id,gender,language,string,depth) values('"+obj.page+"','"+obj.element_id+"','"+obj.gender+"','"+obj.language+"','"+obj.string+"',"+obj.depth+");";
+	insert = insert.replace("'NULL'","null");
+	console.log(insert);
+	await client.query(insert);
+}
+
+module.exports = { getContent,createContent };
--- app/index.js
+++ app/index.js
@@ -3,12 +3,38 @@ const http = require('http');
 const fs = require('fs').promises;
 const hostname = 'localhost';
 const port = 3000;
+const password = process.env.PASSWORD;
 
 async function getFile(file) {
 	return await fs.readFile(file,'utf8');
 }
 
+async function buildAdmin(html, content) {
+	table = "<table><tr>";
+	fields = content.fields.map(field=>field.name);
+	table += fields.map(field => "<th>"+field+"</th>").join("");
+	table += "</tr>";
+	rows = content.rows;
+	table += rows.map(row => "<tr><td>"+row.content_id+"</td><td>"+row.page+"</td><td>"+row.element_id+"</td><td>"+row.gender+"</td><td>"+row.language+"</td><td>"+row.string+"</td><td>"+row.depth+"</td></tr>").join("");
+	return html.replace('[[table]]',table);
+}
+
+function persist(data) {
+	data = data.split("&");
+	if(data[6].split("=")[1] != password) return;
+	obj = {};
+	obj.page = data[0].split("=")[1];
+	obj.element_id = data[1].split("=")[1];
+	obj.gender = data[2].split("=")[1];
+	obj.language = data[3].split("=")[1];
+	obj.string = data[5].split("=")[1];
+	obj.depth = data[4].split("=")[1];
+	db.createContent(obj);
+}
+
 function buildScript(html, map) {
+	deepest = map.map(row=>row.depth).reduce((largest,current)=>largest>current?largest:current);
+	sortedMap = map.sort((a,b)=>a.depth-b.depth);
 	genders = new Set(map.map(item=>item.gender).filter(Boolean));
 	languages = new Set(map.map(item=>item.language).filter(Boolean));
 	string = "";
@@ -27,8 +53,22 @@ const server = http.createServer(async (req, res) => {
 	else url = req.url;
 	content = await db.getContent();
 	try {
-		html = await getFile(__dirname + url);
-		html = buildScript(html,content);
+		if(url == "/save") {
+			let body = '';
+			req.on('data',buffer=>{body+=buffer.toString();});
+			req.on('end',()=>{
+				persist(body);
+				res.end("ok")
+			});
+		} else {
+			html = await getFile(__dirname + url);
+			if(url == "/index.html")
+				html = buildScript(html,content.rows);
+			else if(url == "/admin.html")
+				html = await buildAdmin(html,content);
+			else if(url == "/save")
+				html = "SAVE";
+		}
 		res.writeHead(200);
 		res.end(html);
 	} catch {
