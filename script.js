8d58368faf9c7b23ef8422166ee5862d825b0f26
Patrícia Villela Ocaña Bruno <patriciavillela@proton.me>
2022-08-10 10:26:57 -0300

    Adiciona livros nas listas e muda o defaul para aberto

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 document.querySelectorAll("h2, h3").forEach(el => {
     el.addEventListener("click", (event) => {
         nextElementSibling = el.nextElementSibling;
-        if(nextElementSibling.style.display === 'block') {
+        if(nextElementSibling.style.display === 'none') {
+            nextElementSibling.style.display = 'block';
+            el.classList.add("aberto");
+            el.classList.remove("fechado");
+        } else {
             nextElementSibling.style.display = 'none';
             el.classList.remove("aberto");
             el.classList.add("fechado");
-        } else {
-            nextElementSibling.style.display = 'block';
-            el.classList.remove("fechado");
-            el.classList.add("aberto");
         }
     })
 })
